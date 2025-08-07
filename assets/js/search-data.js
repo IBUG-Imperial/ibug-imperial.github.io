a427d2c3e0d72a1f5671e53aa2001b3cc355150a
IBUG-Imperial <ibug.imperial@gmail.com>
2025-08-07 20:32:57 +0000

    Deploying to gh-pages from @ IBUG-Imperial/ibug-imperial.github.io@5af32002c8938d0ae664a4ef75a2309092be0da6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,21 +14,21 @@ ninja.data = [{
           description: "publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Publications/";
+            window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "Research projects.",
+        },{id: "nav-team",
+          title: "Team",
+          description: "Mobile Vision Perception (MVP) Lab",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Projects/";
+            window.location.href = "/projects/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Teaching/";
+            window.location.href = "/teaching/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
@@ -70,51 +70,21 @@ ninja.data = [{
           section: "News",},{id: "news-welcome-to-face-anti-spoofing-challenge-iccv-2025",
           title: 'Welcome to Face Anti-spoofing Challenge @ ICCV 2025.',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-ronglai-zuo",
+          title: 'Ronglai Zuo',
+          description: "Sign Language Translation/Generation",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/RonglaiZuo/";
+            },},{id: "projects-roy-miles",
+          title: 'Roy Miles',
+          description: "Multimodal LLMs",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/RoyMiles/";
+            },},{id: "projects-yanzuo-lu",
+          title: 'YanZuo Lu',
+          description: "Generative Diffusion Models",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/YanzuoLu/";
             },},{
         id: 'social-email',
         title: 'email',
